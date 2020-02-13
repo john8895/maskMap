cc721505321328d1902c45d588cda05bbcdcaf03
JohnChen <paiching01@gmail.com>
2020-02-13 22:42:32 +0800

    data filter edit

--- js/all.js
+++ js/all.js
@@ -31,7 +31,7 @@ const dataJson = {
                 "name": "德川中西藥局",
                 "phone": "02 -27683399",
                 "address": "台北市松山區八德路4段96號",
-                "mask_adult": 2,
+                "mask_adult": 0,
                 "mask_child": 46,
                 "updated": "2020\/02\/09 14:36:40",
                 "available": "星期一上午看診、星期二上午看診、星期三上午看診、星期四上午看診、星期五上午看診、星期六上午休診、星期日上午休診、星期一下午看診、星期二下午看診、星期三下午看診、星期四下午看診、星期五下午看診、星期六下午休診、星期日下午看診、星期一晚上看診、星期二晚上看診、星期三晚上看診、星期四晚上看診、星期五晚上看診、星期六晚上休診、星期日晚上休診",
@@ -54,8 +54,8 @@ const dataJson = {
                 "name": "家音藥局",
                 "phone": "02 -37652080",
                 "address": "台北市松山區民生東路5段73號",
-                "mask_adult": 196,
-                "mask_child": 51,
+                "mask_adult": 0,
+                "mask_child": 0,
                 "updated": "2020\/02\/09 14:36:40",
                 "available": "星期一上午看診、星期二上午看診、星期三上午看診、星期四上午看診、星期五上午看診、星期六上午看診、星期日上午看診、星期一下午看診、星期二下午看診、星期三下午看診、星期四下午看診、星期五下午看診、星期六下午看診、星期日下午看診、星期一晚上看診、星期二晚上看診、星期三晚上看診、星期四晚上看診、星期五晚上看診、星期六晚上看診、星期日晚上看診",
                 "note": "早上9:00到晚上11:00",
@@ -78,7 +78,7 @@ const dataJson = {
                 "phone": "02 -27155691",
                 "address": "台北市松山區民生東路4段80巷5號",
                 "mask_adult": 0,
-                "mask_child": 6,
+                "mask_child": 60,
                 "updated": "2020\/02\/09 14:36:40",
                 "available": "星期一上午看診、星期二上午看診、星期三上午看診、星期四上午看診、星期五上午看診、星期六上午看診、星期日上午看診、星期一下午看診、星期二下午看診、星期三下午看診、星期四下午看診、星期五下午看診、星期六下午看診、星期日下午看診、星期一晚上看診、星期二晚上看診、星期三晚上看診、星期四晚上看診、星期五晚上看診、星期六晚上看診、星期日晚上看診",
                 "note": "",
@@ -100,8 +100,8 @@ const dataJson = {
                 "name": "中美藥局",
                 "phone": "02 -27627468",
                 "address": "台北市松山區富錦街531號",
-                "mask_adult": 0,
-                "mask_child": 13,
+                "mask_adult": 130,
+                "mask_child": 0,
                 "updated": "2020\/02\/09 14:36:40",
                 "available": "星期一上午看診、星期二上午看診、星期三上午看診、星期四上午看診、星期五上午看診、星期六上午看診、星期日上午看診、星期一下午看診、星期二下午看診、星期三下午看診、星期四下午看診、星期五下午看診、星期六下午看診、星期日下午看診、星期一晚上看診、星期二晚上看診、星期三晚上看診、星期四晚上看診、星期五晚上看診、星期六晚上看診、星期日晚上看診",
                 "note": "營業時間如有異動,以藥局公告為準",
@@ -148,11 +148,9 @@ getTodat();
 // getMap();
 updateStoreList('all');
 
-// const maskFilter = btnShowMask();
-
-function my$(id) {
-    return document.getElementById(id);
-}
+// function my$(id) {
+//     return document.getElementById(id);
+// }
 
 // 取得今天日期
 function getTodat() {
@@ -164,20 +162,17 @@ function getTodat() {
 // 成人、兒童口罩篩選
 function btnShowMask() {
     console.log("btnShowMask");
-    let maskBtn = my$('fliterMask').getElementsByTagName('input');
+    let maskBtn = document.getElementById('fliterMask').getElementsByTagName('input');
     for (let i = 0; i < maskBtn.length; i++) {
         maskBtn[i].onclick = function () {
             switch (maskBtn[i].id) {
                 case 'btn__adultMask':
-                    // showMask = "adult";
                     updateStoreList('adult');
                     break;
                 case 'btn__childMask':
-                    // showMask = "child";
                     updateStoreList('child');
                     break;
                 default:
-                    // showMask = "all";
                     updateStoreList('all');
             }
         }
@@ -186,23 +181,25 @@ function btnShowMask() {
 
 btnShowMask();
 
-// 篩選口罩數量 2020.02.12
+// 篩選口罩數量，更新左側列表
 function updateStoreList(showMask) {
     console.log("updateStoreList");
-    // my$('storeList').innerHTML = "";
-    // console.log(my$('storeList').innerHTML);
+    // TODO: 2020.02.13 這裡要改成 if(item.properties.mask_adult!==0)
     let str = "";
-    // 所有口罩：預設顯示
-    // 成人口罩：顯示成人口罩數量不等於 0 的店家
-    // 2020.02.13 編輯至此 ==============================================
     let filterMask = data.filter(function (item, index, array) {
-        if (showMask && showMask == 'adult') {
-            return item.properties.mask_adult;
+        if(showMask){
+            switch (showMask) {
+                case 'adult':
+                    return item.properties.mask_adult;
+                    break;
+                case 'child':
+                    return item.properties.mask_child;
+                    break;
+                default:
+                    return item;
+            }
         }
-        // return item.properties.showMask !== 0;       // 成人口罩：顯示成人口罩數量不等於 0 的店家
     });
-    // 兒童口罩：顯示兒童口罩數量不等於 0 的店家
-    // info.mask_child !=0
 
     for (let i = 0; i < filterMask.length; i++) {
         let storeStr = `<article>
@@ -213,22 +210,22 @@ function updateStoreList(showMask) {
 <!--                <li>今日營業時間：${filterMask[i].properties.available}</li>-->
             </ul>
         <div class="sidebar__maskNum">
-            <div><em>成人</em> ${filterMask[i].properties.mask_adult}</div>
-            <div><em>兒童</em> ${filterMask[i].properties.mask_child}</div>
-            <span class="sidebar__updateTime">
+            <div class="mask-type mask-adult"><em>成人</em> ${filterMask[i].properties.mask_adult}</div>
+            <div class="mask-type mask-child"><em>兒童</em> ${filterMask[i].properties.mask_child}</div>
+            
+        </div>
+        <span class="sidebar__updateTime">
             ${filterMask[i].properties.updated} 更新
         </span>
-        </div>
               </article>`;
         str += storeStr
     }
-    my$('storeList').innerHTML = str;
-
+    document.getElementById('storeList').innerHTML = str;
+    // generateMarker('true');
 }
 
 // 偵測使用者定位，再產生地圖
 navigator.geolocation.getCurrentPosition(getPosition);
-
 function getPosition(position) {
     let myLocation = [position.coords.latitude, position.coords.longitude];
     getMap(myLocation);
@@ -240,7 +237,8 @@ function getMap(myLocation) {
         center: [25.053144, 121.544704],
         zoom: 16
     });
-
+    const getMapSuccess = true;
+    // 註冊API
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 20,
@@ -248,6 +246,59 @@ function getMap(myLocation) {
         accessToken: 'pk.eyJ1Ijoiam9objg4OTUiLCJhIjoiY2s2ZjBpbnRlMDQxNTNlbXQzcGRmd3g0ZSJ9.CZhZ2p1dMuFgIPard8rqwg'
     }).addTo(map);
 
+
+    generateMarker(getMapSuccess,map);
+    // L.marker(myLocation ? myLocation : [25.061285, 121.565481], {icon: redIcon}).addTo(map)
+    //     .bindPopup('<h1>我的位置</h1>');
+
+    // 調用遠端資料用
+    // var xhr = new XMLHttpRequest();
+    // xhr.open("get","https://raw.githubusercontent.com/kiang/pharmacies/master/json/points.json");
+    // xhr.send();
+    // xhr.onload = function () {
+    //     var data = JSON.parse(xhr.responseText).features;
+    //     for(let i=0;i<data.length;i++){
+    //
+    //     }
+    // }
+    // 欄位
+    // info.name
+    // info.phone
+    // info.address
+    // info.mask_adult
+    // info.mask_child
+    // info.updated
+    // info.available
+    // info.note
+    //
+    // map原生資訊
+    // 與目前位置距離
+    // 目前地址
+    // 地圖
+    //     判斷：
+    //     當前位置：紅色圖示、店家點（綠色）
+    // infoWindow
+    // 店名、距離當前幾km
+    // 導航連結
+    // *標星號
+    // 側邊欄
+    //     顯示字串：最後一碼…
+    //     當前位置：顯示地址
+    //     輸入地址 查詢
+    //     列表項目
+    //         店名、距離當前幾km
+    //         地址
+    //         電話（點選可撥打）
+    //         營業時間
+    //         口罩數量：成人 0 、兒童 20
+    //         *標星號
+    //         *導航連結
+    //         距離最近：離當前位置最近：取得一定範圍內所有marker，計算與當前位置之間的差距，取最小
+    //         存量最多：計算一定範圍內所有marker，口罩總數（成人+兒童）最多，取最大
+    //         *已標星號
+    //
+}
+function generateMarker(getMapSuccess, map){
     // ICON
     let greenIcon = new L.Icon({
         iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
@@ -281,85 +332,23 @@ function getMap(myLocation) {
         popupAnchor: [1, -34],
         shadowSize: [41, 41]
     });
-
-    // L.marker(myLocation ? myLocation : [25.061285, 121.565481], {icon: redIcon}).addTo(map)
-    //     .bindPopup('<h1>我的位置</h1>');
-
     // 群組 marker
+    if(!getMapSuccess){
+        return;
+    }
     let markers = new L.MarkerClusterGroup().addTo(map);
-
-    // 調用遠端資料用
-    // var xhr = new XMLHttpRequest();
-    // xhr.open("get","https://raw.githubusercontent.com/kiang/pharmacies/master/json/points.json");
-    // xhr.send();
-    // xhr.onload = function () {
-    //     var data = JSON.parse(xhr.responseText).features;
-    //     for(let i=0;i<data.length;i++){
-    //
-    //     }
-    // }
-    //
     // 循環取得遠端資料
     for (let i = 0; i < data.length; i++) {
         let lat = data[i].geometry.coordinates[1];
         let lng = data[i].geometry.coordinates[0];
         let info = data[i].properties;
-        // 如果口罩數量為0，則不顯示
+        // 如果口罩數量為0，則不顯示marker
         if (info.mask_adult + info.mask_child === 0) {
             continue;
-        }
-        // 欄位
-        // info.name
-        // info.phone
-        // info.address
-        // info.mask_adult
-        // info.mask_child
-        // info.updated
-        // info.available
-        // info.note
-        //
-        // map原生資訊
-        // 與目前位置距離
-        // 目前地址
-        //
-        // 地圖
-        //     判斷：
-        //     當前位置：紅色圖示、店家點（綠色）
-        // infoWindow
-        // 店名、距離當前幾km
-        // 導航連結
-        // *標星號
-        // 側邊欄
-        //     顯示字串：最後一碼…
-        //     當前位置：顯示地址
-        //     輸入地址 查詢
-        //     列表項目
-        //         店名、距離當前幾km
-        //         地址
-        //         電話（點選可撥打）
-        //         營業時間
-        //         口罩數量：成人 0 、兒童 20
-        //         *標星號
-        //         *導航連結
-
-        //         距離最近：離當前位置最近：取得一定範圍內所有marker，計算與當前位置之間的差距，取最小
-        //         存量最多：計算一定範圍內所有marker，口罩總數（成人+兒童）最多，取最大
-        //         *已標星號
-        //
-        switch (showMask) {
-            case 'adult':
-                if (info.mask_adult === 0) {
-                    break;
-                }
-            case 'child':
-                if (info.mask_child === 0) {
-                    break;
-                }
         }
         markers.addLayer(L.marker([lat, lng], {icon: info.mask_adult + info.mask_child !== 0 ? blueIcon : greyIcon})
             .bindPopup("<h1>" + info.name + "</h1><em>" + info.updated + " 更新</em><div class='map__maskNum'><div><em>成人</em> " + info.mask_adult + "</div><div><em>兒童</em> " + info.mask_child + "</div></div>"))
             .openPopup();
     }
-    // map.addLayer(markers);
+    map.addLayer(markers);
 }
-
